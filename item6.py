cb2bd47fee87729d55a8debcdbed3b71cbbed857
Martin <73581070+clairvoyant-xo@users.noreply.github.com>
2023-11-21 15:47:10 -0300

    Add files via upload

--- item6.py
+++ item6.py
@@ -8,48 +8,33 @@ def u(n):
 def pulso_glotico(t,p0,tp,tn):
     return (0.5 * p0) * (1 - np.cos(t/tp * np.pi)) * (u(t) - u(t - tp)) + p0 * np.cos((t-tp)/tn * np.pi/2) * (u(t - tp) - u(t - (tp+tn)))
 
-def tren_pulsos_gloticos(t,p0,tp,tn,n,f):
-    suma = 0
-    for i in range(0,n):
-        suma += pulso_glotico(t - i/f,p0,tp,tn)
-    return suma
-
-def muestrear_pulsos_gloticos(p0,tp,tn,n,f,t0,tf,fs):
-    muestras = (int) ((tf - t0) * fs)
-    x = np.zeros(muestras)
-    for i in range(0,muestras):
-        x[i] = tren_pulsos_gloticos(t0 + i/fs,p0,tp,tn,n,f)
-    return x,muestras
+def muestrear_pulsos_gloticos(t0,p0,tp,tn,fs,n):
+    longitud_pulso = (int) (t0 * fs)
+    x = np.empty(longitud_pulso)
+    for i in range(0,longitud_pulso):
+        x[i] = pulso_glotico(i/fs,p0,tp,tn)
+    return np.tile(x,n)
 
 def calcular_fft_pulsos(fs,x):
     longitud_muestra = len(x)
     X = fft.fft(x)
     f = np.arange(0,longitud_muestra,1) * fs / (longitud_muestra) - fs / 2
 
-    return fft.fftshift(np.abs(X)),f
+    return fft.fftshift(X),f
 
 f0 = 200
 p0 = 1
 tp = 0.4 * 1/f0
 tn = 0.16 * 1/f0
-k = 20
+k = 10
 t0 = -0.05
 tf = 0.15
 fs = 16e3
 
-t = np.linspace(t0,tf,1000)
+x = muestrear_pulsos_gloticos(1/f0,p0,tp,tn,fs,k)
+n = np.arange(0,len(x),1)
 
 plt.figure(1)
-plt.plot(t,tren_pulsos_gloticos(t,p0,tp,tn,k,f0))
-plt.title('Gráfico de pulsos glóticos')
-plt.xlabel('Tiempo [s]')
-plt.ylabel('Amplitud')
-plt.grid()
-
-x, n = muestrear_pulsos_gloticos(p0,tp,tn,k,f0,t0,tf,fs)
-n = np.arange(0,n,1)
-
-plt.figure(2)
 plt.title('Muestreo de pulsos glóticos')
 plt.xlabel('n')
 plt.ylabel('Amplitud')
@@ -58,11 +43,11 @@ plt.grid()
 
 X, f = calcular_fft_pulsos(fs,x)
 
-plt.figure(3)
+plt.figure(2)
 plt.title('Amplitud de FFT de pulsos glóticos')
 plt.xlabel('Frecuencia [Hz]')
 plt.ylabel('Amplitud')
-plt.stem(f,X,markerfmt=' ',basefmt="gray")
+plt.stem(f,np.abs(X),markerfmt=' ',basefmt="gray")
 plt.grid()
 
 plt.show()
---
+++ item8.py
@@ -0,0 +1,88 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sgn
+import scipy.fft as fft
+import scipy.io.wavfile as wav
+
+def u(n):
+    return np.heaviside(n,1)
+
+def pulso_glotico(t,p0,tp,tn):
+    return (0.5 * p0) * (1 - np.cos(t/tp * np.pi)) * (u(t) - u(t - tp)) + p0 * np.cos((t-tp)/tn * np.pi/2) * (u(t - tp) - u(t - (tp+tn)))
+
+def muestrear_pulsos_gloticos(t0,p0,tp,tn,fs,n):
+    longitud_pulso = (int) (t0 * fs)
+    x = np.empty(longitud_pulso)
+    for i in range(0,longitud_pulso):
+        x[i] = pulso_glotico(i/fs,p0,tp,tn)
+    return np.tile(x,n)
+
+def calcular_fft_pulsos(fs,x):
+    longitud_muestra = len(x)
+    X = fft.fft(x)
+    f = np.arange(0,longitud_muestra,1) * fs / (longitud_muestra) - fs / 2
+
+    return fft.fftshift(X),f
+
+def p(F,B,fs):
+    return np.exp(-2 * np.pi * B/fs) * np.exp(2j * np.pi * F/fs)
+
+def polos_vocal(vocal,fs):
+    polos = np.empty(8,dtype=np.clongdouble)
+    for i in range(0,4):
+        polos[2*i] = p(vocal[0][i],vocal[1][i],fs)
+        polos[2*i+1] = np.conj(polos[2*i])
+    return polos
+
+def forma_sos_filtro(polos):
+    sos = np.empty([4,6])
+    for i in range(0,4):
+        sos[i][0] = 1
+        sos[i][1] = 0
+        sos[i][2] = 0
+        sos[i][3] = 1
+        sos[i][4] = -2 * np.real(polos[2*i])
+        sos[i][5] = np.abs(polos[2*i])**2
+    return sos
+
+fs = 16e3
+f0 = 200
+p0 = 1
+tp = 0.4 * 1/f0
+tn = 0.16 * 1/f0
+k = 200
+
+vocal_a = [[830,1400,2890,3930],[110,160,210,230]]
+vocal_e = [[500,2000,3130,4150],[80,156,190,220]]
+vocal_i = [[330,2765,3740,4366],[70,130,178,200]]
+vocal_o = [[546,934,2966,3930],[97,130,185,240]]
+vocal_u = [[382,740,2760,3380],[74,150,210,180]]
+
+p = polos_vocal(vocal_a,fs)
+sos = forma_sos_filtro(p)
+
+x = muestrear_pulsos_gloticos(1/f0,p0,tp,tn,fs,k)
+n = np.arange(0,len(x),1)
+
+filtro = sgn.sosfilt(sos,x)
+
+fs, audio = wav.read("./hh15.wav")
+wav.write("./vocal.wav",fs,filtro.astype(audio.dtype))
+
+plt.figure(1)
+plt.title('Pulso glótico filtrado')
+plt.xlabel('n')
+plt.ylabel('Amplitud')
+plt.stem(n,filtro,markerfmt=' ',basefmt="gray")
+plt.grid()
+
+X, f = calcular_fft_pulsos(fs,filtro)
+
+plt.figure(2)
+plt.title('Amplitud de FFT del pulso glótico filtrado')
+plt.xlabel('Frecuencia [Hz]')
+plt.ylabel('Amplitud')
+plt.stem(f,np.abs(X),markerfmt=' ',basefmt="gray")
+plt.grid()
+
+plt.show()
